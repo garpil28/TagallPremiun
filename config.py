07527cf8b2ea2a679d630fe4078a8aa7867ebe79
garpil28 <storegarfield076@gmail.com>
2025-10-29 01:52:22 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# === CONFIG.PY ===
+# Informasi umum tentang bot dan pemilik
+
+OWNER_USERNAME = "garfil28"          
+OWNER_NAME = "Garfield Store"       
+BOT_NAME = "Auto TagAll Premium"     
+VERSION = "2.0"                      
