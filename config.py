4adafc733b3e0db3d055419df3b1cf4d6302b1c0
garpil28 <storegarfield076@gmail.com>
2025-10-29 20:18:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
-# ===== CONFIG BOT =====
+# =========================
+# CONFIG BOT AUTOTAGALL
+# =========================
+
 OWNER_ID = 6954401932
 OWNER_USERNAME = "kopi567"
 OWNER_NAME = "Garfield Store"
-BOT_NAME = "Auto TagAll Premium"
-VERSION = "3.0"
-BOT_TOKEN = "MASUKKAN_BOT_TOKEN_BOTFATHER"
-API_ID = "MASUKKAN_API_ID"
-API_HASH = "MASUKKAN_API_HASH"
-            
+BOT_NAME = "AutoTagAll Premium"
+VERSION = "2.0"
+
+BOT_TOKEN = "8487458549:AAFD4xmraiWBo7OFtlwx4kdPHPtD9A0g9jE"  
+API_ID = "25228455"    
+API_HASH = "a4b300359ac029aafe51855ec85b40b1"  
+
+DB_PATH = "database.db"
+LOG_DIR = "logs"
+
