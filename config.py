a7ee9fdda6906dc90b8993fd4387bb3b9d36412a
garpil28 <storegarfield076@gmail.com>
2025-10-30 14:01:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ OWNER_USERNAME  = "kopi567"
 OWNER_IDS       = [6954401932, 5727852338]
 
 # ===== LOG CONFIG =====
-LOG_GROUP_ID    = -1003282574590
+LOGS_CHAT_ID    = -1003282574590
 LOG_FILE_NAME   = "BotTagallGarfieldLogs.txt"
 
 # ===== GEMINI / API =====
@@ -17,4 +17,3 @@ GEMINI_API_KEY  = ""
 SUPPORT_GROUP   = "https://t.me/garfieldoffc"
 UPDATES_CHANNEL = "storegarf"
 BANNER_IMG_URL  = "https://files.catbox.moe/vcdo1g.jpg"
-
