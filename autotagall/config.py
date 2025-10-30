dbd425add3239607222914457c31e6747f11d354
garpil28 <storegarfield076@gmail.com>
2025-10-31 03:16:47 +0700

    Create config.py

---
+++ autotagall/config.py
@@ -0,0 +1,6 @@
+import os
+
+# Ganti dengan data kamu sendiri
+API_ID = int(os.environ.get("API_ID", 123456))  # dapat dari https://my.telegram.org
+API_HASH = os.environ.get("API_HASH", "isi_api_hash_kamu")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "isi_token_bot_kamu")
