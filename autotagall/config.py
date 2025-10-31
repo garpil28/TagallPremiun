f4f380b3874bccec0fa13b98dbdaa00ae44487d0
garpil28 <storegarfield076@gmail.com>
2025-10-31 19:13:27 +0700

    Update config.py

--- autotagall/config.py
+++ autotagall/config.py
@@ -1,6 +1,27 @@
-import os
+# =========================================================
+# 🧠 CONFIG FILE - AutoTagAll Premium
+# =========================================================
 
-# Ganti dengan data kamu sendiri
-API_ID = int(os.environ.get("API_ID", 123456))  # dapat dari https://my.telegram.org
-API_HASH = os.environ.get("API_HASH", "isi_api_hash_kamu")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "isi_token_bot_kamu")
+# ===== BOT CONFIG =====
+BOT_NAME = "AutoTagAll Premium"
+BOT_TOKEN = "7665260692:AAFjY00PFEQKWslWifvlqFoQCEIiZb543j8"
+
+# ===== OWNER CONFIG =====
+OWNER_NAME = "kopi567"
+OWNER_USERNAME = "kopi567"
+OWNER_IDS = [6954401932, 5727852338]
+
+# ===== LOG CONFIG =====
+LOGS_CHAT_ID = -1003282574590
+LOG_FILE_NAME = "BotTagallGarfieldLogs.txt"
+
+# ===== SUPPORT & CHANNEL =====
+SUPPORT_GROUP = "https://t.me/garfieldoffc"
+UPDATES_CHANNEL = "storegarf"
+BANNER_IMG_URL = "https://files.catbox.moe/vcdo1g.jpg"
+
+# ===== TIMEZONE CONFIG =====
+TIMEZONE = "Asia/Jakarta"  # WIB (GMT+7)
+
+# ===== VERSION INFO =====
+BOT_VERSION = "v1.0.0"
